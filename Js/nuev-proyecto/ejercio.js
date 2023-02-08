88494058b9c243aebee2c888f70eaf618a2603b7
beasteve <beasteve43@gmail.com>
2023-02-08 14:34:31 +0100

    last 8th febrero commit

--- Js/nuev-proyecto/ejercio.js
+++ Js/nuev-proyecto/ejercio.js
@@ -1,6 +1,6 @@
-var a = 2;
-var b = 4;
-var c = 6;
+// var a = 2;
+// var b = 4;
+// var c = 6;
 
 // console.log(a+b+c);
 
@@ -8,18 +8,86 @@ var c = 6;
 // var texto = 2;
 // var a = "true";
 
-
 // console.log(parseInt(texto));
 // console.log(numero.toString());
 // console.log(Boolean(a))
-
 
 // var food = "Rice";
 
 // var food = "Patata"
 
+// console.log(28*3);
 
-console.log(2%3);
+// let gt = 3;
+// console.log(++gt)
 
-let gt = 3;
-console.log(++gt)
+// let str = ("2" * 3);
+
+// let world = "Hello Todos "
+// document.write(`<h1>${ world+str  }</h1>`);
+
+// let j = 4;
+// let k = 4;
+
+// if(j> k){
+//     console.log(`j ${j} es moyer que k (${k})`);
+//     document.write(`j ${j} es moyer que k (${k})`);
+// } else if(j<k){
+//     console.log(`k ${k} es moyer que k (${j})`);
+//     document.write(`k ${k} es moyer que k (${j})`);
+// } else if( j === k){
+//     console.log(`k ${j} es igual y pose el mismo tipo que k (${k})`)
+//     document.write(`k ${j} es igual y pose el mismo tipo que k (${k})`)
+// }
+
+// let a=1, b=3, c=4, d=5;
+
+// if(a+b > c +d){
+
+// }
+
+// (a, b, c y d)
+
+// Write a program that stores three angles of a triangle in integer variables
+// (angle1, angle2, and angle3). Create a conditional that tests whether these
+// three angles together can form a triangle (the angles of a triangle always add up to 180 degrees).
+
+// let angule1= 61, angule2 = 60, angule3 =60;
+
+// if(angule1 + angule2 + angule3 === 180){
+//     document.write("<h1> It have has form triangle of 180</h1>");
+
+// } else{
+//     document.write("<h1>Try again Chico!</h1>");
+//     alert('Wohoho Hombre !')
+// }
+
+// let day = new Date();
+// // let time = day.getDay();
+// let month = day.getMonth();
+
+// switch (month) {
+//   case 4:
+//     document.write("Enero");
+//     break;
+//   case 2:
+//     document.write("Febrero");
+//     break;
+//   case 3:
+//     document.write("Marzo");
+//     break;
+//   default:
+//     document.write("No day exits");
+//     break;
+// }
+
+
+for(let i = 0; i<50;  i++){
+    console.log(i)
+    document.write(i)
+}
+
+for(let i = 0; i<11; i++){
+    document.write(i * 5 )
+}
+
